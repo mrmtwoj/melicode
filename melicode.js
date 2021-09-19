3e121e2dee51865b9ca6e5a671486008b3502036
Mr Mtwoj <mr.mtwoj@gmail.com>
2021-09-19 12:25:24 +0430

    Create melicode.js

---
+++ melicode.js
@@ -0,0 +1,27 @@
+function melicode_check() {
+            var melicode = document.getElementById("melicode").value;
+            document.getElementById('meli_er').style.display='none';
+            function vmsNationalCode(input) {
+                if (!/^\d{10}$/.test(input)
+                    || input=='0000000000'
+                    || input=='1111111111'
+                    || input=='2222222222'
+                    || input=='3333333333'
+                    || input=='4444444444'
+                    || input=='5555555555'
+                    || input=='6666666666'
+                    || input=='7777777777'
+                    || input=='8888888888'
+                    || input=='9999999999')
+                    return false;
+                var check = parseInt(input[9]);
+                var sum = 0;
+                var i;
+                for (i = 0; i < 9; ++i) {
+                    sum += parseInt(input[i]) * (10 - i);
+                }
+                sum %= 11;
+                return (sum < 2 && check == sum) || (sum >= 2 && check + sum == 11);
+            }
+            if (vmsNationalCode(melicode) === false) document.getElementById('meli_er').style.display='block';
+        }
